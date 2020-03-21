a9404f912e65178a65d0f154b0d0230b51f04788
Juan Castillo <Juanpablodlcc@gmail.com>
2020-03-21 16:45:58 -0600

    encapsulando adn

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
 import React, { Component } from 'react';
 import ADN from './api/biology/index'
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = { ADN: "" }
-  }
-
-  handleChange({ target }) {
-    console.log(target)
-    this.setState({ ADN: target.value.replace(/ |-/g,"") })
-  }
-
   render() {
-    return (
-      <div className="App" >
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Bienvenido a <em>Biologiapi</em>
-          </p>
-          <br />
-          {
-            this.state.ADN.length ?
-              <ADN ADN={this.state.ADN} /> :
-              <h1>Ingresa una secuencia de ADN</h1>
-          }
-          <input
-            className="input"
-            type="text"
-            placeholder="recuerda ingresar una base iniciadora, por ej: 'Tac'..."
-            onChange={this.handleChange.bind(this)}
-          />
-        </header>
-      </div>
-    );
+    return (<ADN/>);
   }
 }
 
--- src/api/biology/index.js
+++ src/api/biology/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { allAboutIt } from './adn'
-
+import logo from '../../logo.svg';
+import '../../App.css';
 
 class ADN extends Component {
     render() {
@@ -28,4 +29,40 @@ class ADN extends Component {
 
 }
 
-export default ADN 
+class App extends Component {
+    constructor() {
+      super();
+      this.state = { ADN: "" }
+    }
+  
+    handleChange({ target }) {
+      console.log(target)
+      this.setState({ ADN: target.value.replace(/ |-/g,"") })
+    }
+  
+    render() {
+      return (
+        <div className="App" >
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Bienvenido a <em>Biologiapi</em>
+            </p>
+            <br />
+            {
+              this.state.ADN.length ?
+                <ADN ADN={this.state.ADN} /> :
+                <h1>Ingresa una secuencia de ADN</h1>
+            }
+            <input
+              className="input"
+              type="text"
+              placeholder="recuerda ingresar una base iniciadora, por ej: 'Tac'..."
+              onChange={this.handleChange.bind(this)}
+            />
+          </header>
+        </div>
+      );
+    }
+  }
+export default App 

e9a406c19657011981e7d5f4da19516db5d52235
Juan Castillo <Juanpablodlcc@gmail.com>
2020-03-22 19:52:55 -0600

    menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
-import ADN from './api/biology/index'
-
+import './App.css'
+import logo from './logo.svg'
+import ADN from './api/biology'
 class App extends Component {
+
+  constructor() {
+    super();
+    this.onClick.bind(this)
+  }
+  onClick({ target }) {
+    console.log(target)
+  }
   render() {
-    return (<ADN/>);
+    return (
+      <>
+        <div className="App" >
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Selecciona en que materias quieres ayuda</h2>
+            <h3><u>Opciones</u></h3>
+            <div>
+              <button type="button" className="btn btn-primary btn-lg" onClick={this.onClick} id="biology">Biologia</button>
+              <button type="button" className="btn btn-danger btn-lg" onClick={this.onClick} id="math">Matematica</button>
+              <br />
+              <button type="button" className="btn btn-warning btn-lg" onClick={this.onClick} id="stadistic">Estadistica</button>
+              <button type="button" className="btn btn-success btn-lg" onClick={this.onClick} id="_">Fisica</button>
+            </div>
+          </header>
+        </div>
+      </>
+    );
   }
 }
 
